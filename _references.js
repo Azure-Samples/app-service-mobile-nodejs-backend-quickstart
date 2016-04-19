c2509ac733fb525d7ee2bd13db144006f3d3d57b
Glenn Gailey <ggailey777@hotmail.com>
2016-04-19 16:54:24 -0700

    Removed files per review

--- _references.js
+++
@@ -1,5 +0,0 @@
-// ----------------------------------------------------------------------------
-// Copyright (c) 2015 Microsoft Corporation. All rights reserved.
-// ----------------------------------------------------------------------------
-
-///<reference path="typings/azure-mobile-apps/azure-mobile-apps.d.ts" />
